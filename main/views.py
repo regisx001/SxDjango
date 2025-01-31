1650d3378920ccef2642c229fa78eb9bea169577
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-31 02:12:24 +0100

    test: test debuging django.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import re
 
+from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+from django.utils.timezone import datetime
 # Create your views here.
+
+
+def home(request:HttpRequest):
+    return HttpResponse("Helllo Django")
+
+
+def hello_there(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
